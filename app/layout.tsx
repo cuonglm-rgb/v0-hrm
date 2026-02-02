f6bb4d4a1ccb614fe5801e08fb13a76416b30c33
cuonglm6 <cuonglm@pamoteam.com>
2026-02-02 10:13:41 +0700

    update logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,23 +13,9 @@ export const metadata: Metadata = {
   description: "Manage your people smarter - Modern HR SaaS Platform",
   // generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/pamo.png",
+    apple: "/pamo.png",
   },
-    generator: 'v0.app'
 }
 
 export default function RootLayout({
